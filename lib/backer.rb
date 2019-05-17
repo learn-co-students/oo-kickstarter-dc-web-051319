770c12128da5e9925ebda8572571299773e51af6
Shujaat Azim <shujaat.azim@gmail.com>
2019-05-17 14:20:19 -0400

    Done.

--- lib/backer.rb
+++ lib/backer.rb
@@ -15,7 +15,8 @@ class Backer
 end
 
 # We decided that if we are going to put an instance of a Class 
-# (in this case, project_obj) as an argument for an instance method, we are going to
-# label it as "something_obj" to indicate that it is an object of the class that
-# we're passing in. This way, it's easier to keep track of what is being passed in,
-# and is more clear that this will result in an infinite-loop type set up.
+# (in this case, project_obj) as an argument for an instance method, 
+# we are going to label it as "something_obj" to indicate that it is 
+# an object of the class that we're passing in. This way, it's 
+# easier to keep track of what is being passed in, and is more clear 
+# that this will result in an infinite-loop type set up.
