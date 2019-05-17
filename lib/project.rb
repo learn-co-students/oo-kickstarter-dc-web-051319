6599028d63a8f7917e8e856c732acfbe4a806b06
villatrue <scarter0313@gmail.com>
2019-05-17 12:01:34 -0400

    Done.

---
+++ lib/project.rb
@@ -0,0 +1,19 @@
+class Project
+
+   attr_reader :backers, :title
+
+   def initialize(title)
+       @title = title
+       @backers = []
+   end
+
+   # def title
+   #     "Project With So Much Amaze"
+   # end
+
+   def add_backer(backer)
+       @backers << backer
+       backer.backed_projects << self
+   end
+
+end
