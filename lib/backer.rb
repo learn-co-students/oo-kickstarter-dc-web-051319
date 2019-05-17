a1d9b3582ca85a8194278cee355678f7c0c82b34
Shujaat Azim <shujaat.azim@gmail.com>
2019-05-17 12:20:41 -0400

    Done.

--- lib/backer.rb
+++ lib/backer.rb
@@ -7,9 +7,15 @@ class Backer
         @backed_projects = []
     end
 
-    def back_project(project)
-        self.backed_projects << project
-        project.backers << self
+    def back_project(project_obj)
+        self.backed_projects << project_obj
+        project_obj.backers << self
     end
 
 end
+
+# We decided that if we are going to put an instance of a Class 
+# (in this case, project_obj) as an argument for an instance method, we are going to
+# label it as "something_obj" to indicate that it is an object of the class that
+# we're passing in. This way, it's easier to keep track of what is being passed in,
+# and is more clear that this will result in an infinite-loop type set up.
